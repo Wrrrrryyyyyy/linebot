cc1dfc329671582c720171e241a6e0eb709a9ebd
Wrrrrryyyyyy <113409883+Wrrrrryyyyyy@users.noreply.github.com>
2024-12-09 19:19:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ line_bot_api = LineBotApi('jKrk+s4/UE56v4zhupFXUagvcGibXCQW84IW4Zy7cEuALZtcCsWnD
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('a1d4fd09fa0d9c5a9d6127d1ac740fff')
 
-line_bot_api.push_message('U66b7a622411ff2975fb04a3d2fb0a8c1', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('U66b7a622411ff2975fb04a3d2fb0a8c1', TextSendMessage(text='您好,目前時間是 2024/10/10 14:00 ，請問需要什麼服務呢?'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -43,8 +43,14 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
+    user_message = event.message.text
+    
+    if user_message == "天氣":
+        reply_message = TextSendMessage(text="請稍等，我幫您查詢天氣資訊！")
+    else:
+        reply_message = TextSendMessage(text="很抱歉，我目前無法理解這個內容。")
+    
+    line_bot_api.reply_message(event.reply_token, reply_message)
 
 #主程式
 import os
